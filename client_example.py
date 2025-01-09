67e2ba567d8b17bdd823ca3ed01e08482ffbec5e
kalixsmurf <onat.oz@metu.edu.tr>
2025-01-09 18:48:47 +0200

    Example usage provided

--- client_example.py
+++ client_example.py
@@ -4,13 +4,37 @@ def test_flask_api():
     url = "http://127.0.0.1:5000/process"
 
     payload = {
-        "path": "/test.mp3"
+        "path": "/test.mp3" #Here, the path that will be sent should be the relative path with respect to the server.py file
     }
+    """
+    For example, if the child safety app(the one with tkinter) is running in a folder named "app"
+    The server.py file is running in a folder named "backend"
+    The ml related files exist in a folder named "ml"
+    And the audio files are recorded directly to the project root
+    So, the overall project file structure is the following:
+    
+    project_root
+    |
+    --> ml
+    |    |
+    |   --> ml files
+    |    
+    --> backend
+    |    |
+    |    --> server.py
+    |    
+    --> app
+    |    |
+    |    --> app.py
+    |    
+    --> sample_audio.wav
+
+    Then, the path that is sent by the client, should be "../sample_audio.wav" as the audio file is in the parent folder of backend folder.
+    """
 
     try:
         response = requests.post(url, json=payload)
 
-        # Print the response
         if response.status_code == 200:
             print("Success:", response.json())
         else:
